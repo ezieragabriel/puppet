38a79cbbf26c2ddff18dc025a9cca6822b0f7ad1
Eziera Gabriel <eziera.gabriel@gmail.com>
2016-05-29 01:33:33 +0800

    Completed code for packages (vim, curl and git) only. Not yet checked for errors.

--- puppet_manifest.pp
+++ puppet_manifest.pp
@@ -2,6 +2,16 @@
 
 # Package class
 class package {
+
   package { 'vim':
   ensure => installed, 		# Ensures that vim is installed
+  }
+
+  package { 'curl':
+  ensure => installed,		# Ensures that curl is installed
+  }
+
+  package { 'git':
+  ensure => installed,		# Ensures that git is installed
+  }
 }

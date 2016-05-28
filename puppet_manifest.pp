0a0640d01bde7a882b8a21fcd647c48cef1750d9
Eziera Gabriel <eziera.gabriel@gmail.com>
2016-05-29 04:52:04 +0800

    Added missing '', change one comment and added 'scripts' directory code and 'exec' resource code.

--- puppet_manifest.pp
+++ puppet_manifest.pp
@@ -19,12 +19,23 @@ class puppet {
   # Create user
   user { 'monitor':		
   ensure => present,		# Ensures that user 'monitor' is present
-  home => /home/monitor,	# Home directory is set to given path
-  shell => /bin/bash,		# User's login shell
+  home => '/home/monitor',	# Home directory is set to given path
+  shell => '/bin/bash',		# User's login shell
+  }
+  
+  # Ensure home directory
+  file { '/home/monitor':
+    ensure => directory,          # Ensures monitor's home directory
   }
 
-  # Create home directory
-  file { '/home/monitor':
-  ensure => directory		# Ensures monitor's home directory
+  # Creates scripts directory
+  file { '/home/monitor/scripts':
+  ensure => directory,
+  }
+  
+  # Downloads memory_check script and store it to scripts directory
+  exec { 'wget --directory-prefix=/home/monitor/scripts https://raw.githubusercontent.com/ezieragabriel/memorycheck/master/memory_check':
+  creates => '/home/monitor/scripts/memory_check',
+  path    => '/usr/bin/'
   }
 }
